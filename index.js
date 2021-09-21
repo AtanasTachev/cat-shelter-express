6828356d4a00bf555bd831d8fa401d3c7aab2946
Atanas Tachev <tachev_a#yahoo.com>
2021-09-22 00:10:52 +0300

    some progress

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-// const route = require('ecpress-route')
+const router = express.Router;
+const path = require('path');
+require ('dotenv/config');
 
+const app = express();
+
+app.engine('hbs', handlebars ({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'Cat Shelter'
+    });
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

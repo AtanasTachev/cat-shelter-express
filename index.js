59e29d6370886ad988b13bacd956e7bc24f23e52
Atanas Tachev <tachev_a#yahoo.com>
2021-09-22 22:24:53 +0300

    some changes

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const express = require('express');
-const handlebars = require('express-handlebars');
-const router = express.Router;
+const env = process.env.NODE_ENV || 'development';
+const catsRoute = require('./routes/catRoutes.js')
+const app = require('express')();
+const config = require('./config/config')[env];
+
+require('./config/express.js')(app);
+
 const mongoose = require('mongoose')
 const path = require('path');
 require ('dotenv/config');
 
-const app = express();
-
-app.engine('hbs', handlebars ({
-    extname: 'hbs'
-}));
-
-app.set('view engine', 'hbs');
-
-app.use(express.static('./public'));
-app.use(express.json());
 
 mongoose.connect(process.env.DB_CONNECTION, () => {
 console.log('Connected to DB...');
@@ -22,35 +16,8 @@ console.log('Connected to DB...');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-const catsRoute = require('./routes/catRoutes.js')
 
-app.use('/cats', catsRoute);
+app.use('/', catsRoute);
 
-app.get('/', (req, res) => {
-    res.render('home', {
-        title: 'Cat Shelter'
-    });
-})
 
-app.get('/add-breed', (req, res) => {
-    res.render('addBreed')
-})
-app.post('/add-cat', (req, res) => {
-    let name = req.body.name;
-    let description = req.body.description;
-    let upload = req.body.upload;
-    let breed = req.body.breed;
-
-    let newCat = new Cat(name, description, upload, breed);
-
-    cats.push(newCat);
-
-    res.redirect('/');
-})
-
-app.get('/add-cat', (req, res) => {
-    res.render('addCat')
-})
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(config.port, () => console.log(`Server running on port ${config.port}`));
--- routes/catRoutes.js
+++ routes/catRoutes.js
@@ -1,14 +1,48 @@
-const express = require('express');
-const router = express.Router;
+// const express = require('express');
+const {Router} = require('express');
 const Cat = require('../models/catModel.js');
 
-router.get('/', async(req, res) => {
-    try{
-        const cats = await Cat.find();
-        res.json(cats);
-    } catch(err) {
-        res.json({message: err});
-    }
-});
+const router = Router();
+
+// router.get('/', async(req, res) => {
+//     const cats = [];
+//     try{
+//         cats = await Cat.find();
+//         res.json(cats);
+//     } catch(err) {
+//         res.json({message: err});
+//     }
+// });
+
+router.get('/', (req, res) => {
+    res.render('home', {
+        title: 'Cat Shelter'
+    });
+})
+
+
+router.get('/add-breed', (req, res) => {
+    res.render('addBreed')
+})
+router.post('/add-cat', (req, res) => {
+    let name = req.body.name;
+    let description = req.body.description;
+    let upload = req.body.upload;
+    let breed = req.body.breed;
+
+    let newCat = new Cat(name, description, upload, breed);
+try {
+    await (newCat).save();
+    res.redirect('/');
+} catch (err) {
+    res.render('add-cat', {
+        message: err
+    })
+}
+})
+
+router.get('/add-cat', (req, res) => {
+    res.render('addCat')
+})
 
 module.exports = router;

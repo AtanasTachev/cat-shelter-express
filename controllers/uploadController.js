75408924ad9b363193ea02188bb52f4e78d45c99
Atanas Tachev <tachev_a#yahoo.com>
2021-10-05 10:18:51 +0300

    some basic changes

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,6 +1,11 @@
 const upload = require('../services/multerSetup').upload;
 const multer = require('../services/multerSetup');
 const express = require('express');
+const router = express.Router();
+
+//const upload = async (req, res) => {
+   // let image = await 
+//}
 
 module.exports.send = async(req, res) => {
     await upload.single('upload');
@@ -8,3 +13,4 @@ module.exports.send = async(req, res) => {
     res.send('ok');
 }
 
+module.exports = router;
--- routes.js
+++ routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const catController = require('./controllers/catController.js');
 const homeController = require('./controllers/homeController.js');
+const uploadController = require('./controllers/uploadController');
 
 const router = express.Router();
 
 router.use(homeController);
 router.use('/', catController);
+router.use('/', uploadController);
 
 module.exports = router;
